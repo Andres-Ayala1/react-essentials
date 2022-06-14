c1cc6b2f32a991923e4ba7a0a91dc9a33e4e4cc9
Andres-Ayala1 <andresayala@utexas.edu>
2022-06-13 20:51:58 -0500

    basic layout of first react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen!!!</h1>
+    </header>
+  );
+}
+const dishes = [
+  "Macaroni and Cheese",
+  "Salmon",
+  "Tofu with Vegetables",
+  "Kimchi Fries",
+  "Viet Iced Coffee"
+];
+
+const dishObjects = dishes.map((dish,i) => ({id: i, title: dish}));
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most delicious food around!</p>
+      <img src="https://github.com/Andres-Ayala1.png" 
+      alt="Headshot of Andres Ayala" 
+      height ={200} />
+      <ul style={{ textAlign: "left"}}>
+        {props.food.map((dish) => (
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer (props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Cindy"/>
+      <Main food={dishObjects}/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
