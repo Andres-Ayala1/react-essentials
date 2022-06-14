62dad39a28b2f619d7c697c17ccbecc6c871afd6
Andres-Ayala1 <andresayala@utexas.edu>
2022-06-14 01:36:10 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React, { useReducer, useState } from "react";
-import './App.css';
+import "./App.css";
 
 // useReducer is used to take in the current state and it returns a new state. "return opposite"
 function App() {
-  const [checked, toggle] = useReducer(
-    (checked) => !checked,
-    false
-  );
-
+  const [checked, toggle] = useReducer((checked) => !checked, false);
 
   return (
-  <>
-    <input 
-    type="checkbox" 
-    value={checked} 
-    //We are able to abstract this logic to the useReducer hook and allow this syntax to be simpler.
-    onChange={toggle} 
-    />
-  <p>{checked ? "checked" : "not checked"}</p>
-  </>
+    <>
+      <input
+        type="checkbox"
+        value={checked}
+        //We are able to abstract this logic to the useReducer hook and allow this syntax to be simpler.
+        onChange={toggle}
+      />
+      <p>{checked ? "checked" : "not checked"}</p>
+    </>
   );
 }
 
