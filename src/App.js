25455b5aaaebb23cdd722a6c76766cafdd7cbc7d
Andres-Ayala1 <andresayala@utexas.edu>
2022-07-10 23:22:02 -0500

    Learning Jest and testing-library

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-// https://api.github.com/users/Andres-Ayala1
-
-// useReducer is used to take in the current state and it returns a new state. "return opposite"
-function App({ login }) {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
-      .then(setData);
-  }, []);
-
-  if (data) {
-    return (
-      <div>
-        <h1>{data.name}</h1>
-        <p>{data.location}</p>
-        <img alt={data.login} src={data.avatar_url} />
-      </div>
-    );
-  }
-
-  return <div>No User Available</div>;
+function App() {
+  return (
+    <div>
+      <h1>Hello React Testing Library</h1>
+    </div>
+  );
 }
 export default App;
